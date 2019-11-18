3ab01d0f0b20ee68471e0c96fff440f7a4b085a1
leo <953086648@qq.com>
2019-11-18 23:02:12 +0800

    comit

---
+++ src/main/java/com/leo/MilkApplication.java
@@ -0,0 +1,16 @@
+package com.leo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Leo
+ */
+@SpringBootApplication
+public class MilkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MilkApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/leo/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.leo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author leo
+ * @date 2019/11/18 22:56
+ */
+@Controller
+public class HelloController {
+
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String test () {
+        return "成功了";
+    }
+}
