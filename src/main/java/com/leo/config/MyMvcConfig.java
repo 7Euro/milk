17d9b5c6c6cf0d6ad8370bbf80d576f5b0b20034
leo <953086648@qq.com>
2019-11-21 00:50:31 +0800

    comit

---
+++ src/main/java/com/leo/config/MyMvcConfig.java
@@ -0,0 +1,13 @@
+package com.leo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author leo
+ * @date 2019/11/20 23:16
+ */
+@Configuration
+public class MyMvcConfig implements WebMvcConfigurer {
+
+}
--- src/main/java/com/leo/controller/UserController.java
+++ src/main/java/com/leo/controller/UserController.java
@@ -1,11 +1,16 @@
 package com.leo.controller;
 
+import com.leo.common.BusinessException;
+import com.leo.common.EmBusinessError;
 import com.leo.common.Result;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.thymeleaf.util.StringUtils;
 
 import javax.servlet.http.HttpSession;
 
@@ -18,12 +23,22 @@ import javax.servlet.http.HttpSession;
 public class UserController {
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    @RequestMapping("/login")
-    public String login(){
-        logger.info("进行登录");
-        return "home";
+    @RequestMapping(value = "/login",method = {RequestMethod.POST})
+    public Result login(@RequestParam(name="userName")String userName,
+                        @RequestParam(name="encrptPassword")String encrptPassword) throws BusinessException {
+        //参数校验
+        if (StringUtils.isEmpty(userName) || StringUtils.isEmpty(encrptPassword)) {
+            throw new BusinessException(EmBusinessError.USER_LOGINERROR);
+        }
+        return Result.create(null);
     }
 
-
+    /**
+     * 跳转到登录页面
+     */
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String login() {
+            return "/login.html";
+    }
 
 }
