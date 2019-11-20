e9a70db7ea023688b99fc3646e943c90d1be6b69
leo <953086648@qq.com>
2019-11-20 18:50:56 +0800

    commit

---
+++ src/main/java/com/leo/config/ShiroConfig.java
@@ -0,0 +1,18 @@
+package com.leo.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+/**
+ * @author zql
+ * @description
+ * @Version: v1.0
+ * @date 2019/11/20 18:47
+ **/
+public class ShiroConfig {
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+
+
+}
