72000da24054339fde450478a29cd09fd7e75f17
leo <953086648@qq.com>
2019-11-22 17:55:32 +0800

    更新基础模块

--- src/main/java/com/leo/common/shiro/AuthRealm.java
+++ src/main/java/com/leo/common/shiro/AuthRealm.java
@@ -6,8 +6,10 @@ import com.leo.domain.Role;
 import com.leo.domain.User;
 import com.leo.service.UserService;
 import org.apache.shiro.authc.*;
+import org.apache.shiro.authc.credential.SimpleCredentialsMatcher;
 import org.apache.shiro.authz.AuthorizationInfo;
 import org.apache.shiro.authz.SimpleAuthorizationInfo;
+import org.apache.shiro.codec.CodecSupport;
 import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.subject.PrincipalCollection;
 import com.leo.common.constant.AdminConst;
@@ -15,6 +17,7 @@ import org.apache.shiro.util.ByteSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.StringUtils;
 
+import javax.annotation.PostConstruct;
 import java.util.Set;
 
 /**
@@ -90,5 +93,24 @@ public class AuthRealm extends AuthorizingRealm {
          * 参数4：固定写法
          */
         return new SimpleAuthenticationInfo(user,user.getPassword(),salt,getName());
+    }
+
+    /**
+     * 自定义密码验证匹配器
+     */
+    @PostConstruct
+    public void initCredentialsMatcher() {
+        setCredentialsMatcher(new SimpleCredentialsMatcher() {
+            @Override
+            public boolean doCredentialsMatch(AuthenticationToken authenticationToken, AuthenticationInfo authenticationInfo) {
+                UsernamePasswordToken token = (UsernamePasswordToken) authenticationToken;
+                SimpleAuthenticationInfo info = (SimpleAuthenticationInfo) authenticationInfo;
+                // 获取明文密码及密码盐
+                String password = String.valueOf(token.getPassword());
+                String salt = CodecSupport.toString(info.getCredentialsSalt().getBytes());
+
+                return equals(ShiroUtil.encrypt(password, salt), info.getCredentials());
+            }
+        });
     }
 }
--- src/main/java/com/leo/controller/UserController.java
+++ src/main/java/com/leo/controller/UserController.java
@@ -1,5 +1,11 @@
 package com.leo.controller;
 
+import com.leo.common.constant.AdminConst;
+import com.leo.common.error.BusinessException;
+import com.leo.common.result.EmBusinessResult;
+import com.leo.common.result.Result;
+import com.leo.common.utils.EntityBeanUtil;
+import com.leo.common.utils.ShiroUtil;
 import com.leo.domain.User;
 import com.leo.service.UserService;
 import org.slf4j.Logger;
@@ -9,7 +15,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * @author leo
@@ -35,50 +43,50 @@ public class UserController {
      * @param valid 验证对象
      * @param user 实体对象
      */
-//    @PostMapping("/save")
-//    @ResponseBody
-//    public Result save(User user) {
-//
-//        // 验证数据是否合格
-//        if (user.getId() == null) {
-//
-//            // 判断密码是否为空
-//            if (user.getPassword().isEmpty() || "".equals(user.getPassword().trim())) {
+    @PostMapping("/save")
+    @ResponseBody
+    public Result save(User user) {
+
+        // 验证数据是否合格
+        if (user.getId() == null) {
+
+            // 判断密码是否为空
+            if (user.getPassword().isEmpty() || "".equals(user.getPassword().trim())) {
+                throw new BusinessException(EmBusinessResult.PSSWORD_NOT_NULL);
+            }
+
+            // 判断两次密码是否一致
+//            if (!user.getPassword().equals(valid.getConfirm())) {
 //                throw new BusinessException(EmBusinessResult.PSSWORD_NOT_NULL);
 //            }
-//
-//            // 判断两次密码是否一致
-////            if (!user.getPassword().equals(valid.getConfirm())) {
-////                throw new BusinessException(EmBusinessResult.PSSWORD_NOT_NULL);
-////            }
-//
-//            // 对密码进行加密
-//            String salt = ShiroUtil.getRandomSalt();
-//            String encrypt = ShiroUtil.encrypt(user.getPassword(), salt);
-//            user.setPassword(encrypt);
-//            user.setSalt(salt);
-//        }
-//
-//        // 判断用户名是否重复
-//        if (userService.repeatByUsername(user)) {
-//            throw new BusinessException(EmBusinessResult.USER_EXIST);
-//        }
-//
-//        // 复制保留无需修改的数据
-//        if (user.getId() != null) {
-//            // 不允许操作超级管理员数据
-//            if (user.getId().equals(AdminConst.ADMIN_ID) &&
-//                    !ShiroUtil.getSubject().getId().equals(AdminConst.ADMIN_ID)) {
-//                throw new BusinessException(EmBusinessResult.NO_ADMIN_AUTH);
-//            }
-//
-//            User beUser = userService.getById(user.getId());
-//            String[] fields = {"password", "salt", "picture", "roles"};
-//            EntityBeanUtil.copyProperties(beUser, user, fields);
-//        }
-//
-//        // 保存数据
-//        userService.save(user);
-//        return Result.SAVE_SUCCESS;
-//    }
+
+            // 对密码进行加密
+            String salt = ShiroUtil.getRandomSalt();
+            String encrypt = ShiroUtil.encrypt(user.getPassword(), salt);
+            user.setPassword(encrypt);
+            user.setSalt(salt);
+        }
+
+        // 判断用户名是否重复
+        if (userService.repeatByUsername(user)) {
+            throw new BusinessException(EmBusinessResult.USER_EXIST);
+        }
+
+        // 复制保留无需修改的数据
+        if (user.getId() != null) {
+            // 不允许操作超级管理员数据
+            if (user.getId().equals(AdminConst.ADMIN_ID) &&
+                    !ShiroUtil.getSubject().getId().equals(AdminConst.ADMIN_ID)) {
+                throw new BusinessException(EmBusinessResult.NO_ADMIN_AUTH);
+            }
+
+            User beUser = userService.getById(user.getId());
+            String[] fields = {"password", "salt", "picture", "roles"};
+            EntityBeanUtil.copyProperties(beUser, user, fields);
+        }
+
+        // 保存数据
+        userService.save(user);
+        return Result.SAVE_SUCCESS;
+    }
 }
